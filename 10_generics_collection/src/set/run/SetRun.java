e2da9a6503ed99c934889fff3ba23e00f0e1964e
ParkMinWoo95 <pmw620927@gmail.com>
2025-02-11 17:05:20 +0900

    20250211 IO

---
+++ 10_generics_collection/src/set/run/SetRun.java
@@ -0,0 +1,12 @@
+package set.run;
+
+import set.service.Setservice;
+
+public class SetRun {
+	public static void main(String[] args) {
+		
+		Setservice service = new Setservice();
+		
+		service.test4();
+	}
+}
---
+++ 12_io/src/serction03/run/ObjectRun.java
@@ -0,0 +1,13 @@
+package serction03.run;
+
+import serction03.service.ObjectService;
+
+public class ObjectRun {
+	public static void main(String[] args) {
+		
+		ObjectService service = new ObjectService();
+		
+		// service.outputMember();
+		service.inputMember();
+	}
+}
